5eef35694b53a7c7ead597524e0f87ff55c1806b
yk-tuturu <waikei.koh@gmail.com>
2023-06-27 14:00:35 +0800

    13.3

--- alienFishGame/Assets/Scripts/Dialogue/commandExtend_bgm.cs
+++ alienFishGame/Assets/Scripts/Dialogue/commandExtend_bgm.cs
@@ -12,7 +12,7 @@ public class commandExtend_bgm : MonoBehaviour
         commandManager.instance.commandData.Add("set11", new Action(set11));
         commandManager.instance.commandData.Add("set12", new Action(set12));
         commandManager.instance.commandData.Add("set13", new Action(set13));
-        commandManager.instance.commandData.Add("set13.4", new Action(set13four));
+        commandManager.instance.commandData.Add("set13.3", new Action(set13three));
         commandManager.instance.commandData.Add("set13.5", new Action(set13half));
         commandManager.instance.commandData.Add("set13.6", new Action(set13six));
     }
@@ -38,10 +38,10 @@ public class commandExtend_bgm : MonoBehaviour
         bgmScript.instance.SetParameter(13);
     }
 
-    public void set13four()
+    public void set13three()
     {
-        Debug.Log("13.4 has been set");
-        bgmScript.instance.SetParameter(13.4f);
+        Debug.Log("13.3 has been set");
+        bgmScript.instance.SetParameter(13.3f);
     }
 
     public void set13half()
